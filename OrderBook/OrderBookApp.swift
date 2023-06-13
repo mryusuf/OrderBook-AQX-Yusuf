3a3983816be4605478a6da6b0712bc426e0a3972
mryusuf <123148968+i-yusuf-mufin@users.noreply.github.com>
2023-06-13 22:50:42 +0700

    Initial Commit

---
+++ OrderBook/OrderBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderBookApp.swift
+//  OrderBook
+//
+//  Created by Indra Permana on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct OrderBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
